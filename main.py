2d1562699d394adf33584adf5203073685d254a4
GuhMascarenhass <gustavo_santos2548@hotmail.com>
2024-09-14 23:26:46 -0300

    Reformulação da logica de introdução das variaveis e tabela tableau simplex

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import numpy as np
-variaveis_list = []
-restricoes_list = []
-max_funcao = input("Escreva a função objetiva")
-for cont in range(0, 10):
-    variaveis_list.append(float(input("Digite a variável: ")))
-    if input("Deseja continuar? (0 sair)") == "0":
+header = []
+header_number = [] 
+max_min = int(input("é um problema de Maximização ou Minimização: (1 ou 0) "))
+i = int(input("Quantas icnonitas "))
+if max_min == 1:
+   for x in range(0, i): 
+        header.append(input("Icnonita:"))
+    if int(input("deseja adiconar mais um? (1 S / 0 N) ")) == 0:
+        header_number.append(int("Apenas o coeficiênte "))
+    if int(input("deseja adiconar mais um? (1 S / 0 N) ")) == 0:
         break
+
+    elif max_min == 0:
+
+for i in range(0, len(funcao_objetiva)):
+    for j in funcao_objetiva[i]:
+        x = funcao_objetiva[i]
